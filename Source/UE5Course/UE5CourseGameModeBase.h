7c6c84dd1aab62afb0fdcc6e7d72b116cf0111c9
Thiago L. Zippinotti <thiago.lozzer@gmail.com>
2023-03-08 13:48:51 -0300

    Initial commit

---
+++ Source/UE5Course/UE5CourseGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE5CourseGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5COURSE_API AUE5CourseGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
